882867aef3b3eda4a73b1a30139a425b66b94ed5
eneax <19818937+eneax@users.noreply.github.com>
2018-05-10 22:30:10 +0200

    Center and style ul

--- app/components/App.js
+++ app/components/App.js
@@ -4,7 +4,7 @@ var Popular = require('./Popular');
 class App extends React.Component {
 	render() {
 		return (
-			<div>
+			<div className='container'>
 				<Popular />
 			</div>
 		)
--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,10 +2,18 @@ var React = require('react');
 
 class Popular extends React.Component {
   render() {
+    var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+
     return (
-      <div>
-        Popular
-      </div>
+      <ul className='languages'>
+        {languages.map(function(lang) {
+          return (
+            <li>
+              {lang}
+            </li>
+          )
+        })}
+      </ul>
     )
   }
 }
