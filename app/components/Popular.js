b9fe26e2a8e3e5e1798cf0279e03ce16d74bcc11
eneax <19818937+eneax@users.noreply.github.com>
2018-05-11 17:16:33 +0200

    Add RepoGrid Component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -21,6 +21,35 @@ function SelectLanguage (props) {
 	)
 }
 
+function RepoGrid (props) {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map(function(repo, index) {
+        return (
+          <li key={repo.name} className='popular-item' >
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img 
+                  className='avatar' 
+                  src={repo.owner.avatar_url}
+                  alt={'Repo for ' + repo.owner.login} />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
 SelectLanguage.propTypes = {
 	selectedLanguage: PropTypes.string.isRequired,
 	onSelect: PropTypes.func.isRequired
@@ -63,6 +92,9 @@ class Popular extends React.Component {
 					selectedLanguage={this.state.selectedLanguage}
 					onSelect={this.updateLanguage}
         />
+        {!this.state.repos 
+          ? <p>Loading</p>
+          : <RepoGrid repos={this.state.repos}/>}
 			</div>
     )
   }
