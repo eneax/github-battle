07c6d8c132dd6cd941730fe66a4e066bf080a908
eneax <19818937+eneax@users.noreply.github.com>
2018-05-11 22:21:13 +0200

    Fix refresh page error

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,18 @@ module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/'
 	},
 	module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
-	},
+  },
+  devServer: {
+    historyApiFallback: true
+  },
 	plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
