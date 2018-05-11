8e71bb2738e86b00b3aac845b8d3269d2906997e
eneax <19818937+eneax@users.noreply.github.com>
2018-05-11 22:29:03 +0200

    Config Switch

--- app/components/App.js
+++ app/components/App.js
@@ -3,6 +3,7 @@ var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
@@ -13,9 +14,14 @@ class App extends React.Component {
 			<Router>
 				<div className='container'>
 					<Nav />
-					<Route exact path='/' component={Home} />
-					<Route path='/battle' component={Battle} />
-					<Route path='/popular' component={Popular} />
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/battle' component={Battle} />
+						<Route path='/popular' component={Popular} />
+						<Route render={function() {
+							return <p>Not Found</p>
+						}} />
+					</Switch>
 				</div>
 			</Router>
 		)
