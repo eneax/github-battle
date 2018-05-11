650880284f1f7d8368bc40ba57bed2c469f17d4c
eneax <19818937+eneax@users.noreply.github.com>
2018-05-11 16:34:10 +0200

    Setup axios in api.js

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,12 @@
 var axios = require('axios');
 
 module.exports = {
-  
+  fetchPopularRepos: function (language) {
+    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+
+    return axios.get(encodeURI)
+      .then(function (response) {
+        return response.data.items;
+      });
+  }
 }
