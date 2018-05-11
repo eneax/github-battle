317eaa29441b68da406ca49ba545607de55f85e7
eneax <19818937+eneax@users.noreply.github.com>
2018-05-11 16:37:55 +0200

    Add repos to initial state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage (props) {
 	var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -29,7 +30,8 @@ class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
     
     this.updateLanguage = this.updateLanguage.bind(this);
