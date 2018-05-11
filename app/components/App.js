5ec902df983327ca840810f341ee9509ae6b66a9
eneax <19818937+eneax@users.noreply.github.com>
2018-05-11 22:09:59 +0200

    Create Home Component

--- app/components/App.js
+++ app/components/App.js
@@ -4,6 +4,7 @@ var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Nav = require('./Nav');
+var Home = require('./Home');
 
 class App extends React.Component {
 	render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
 			<Router>
 				<div className='container'>
 					<Nav />
+					<Route exact path='/' component={Home} />
 					<Route path='/popular' component={Popular} />
 				</div>
 			</Router>
