c3deeefff70cc274cb09fac2036c22af962a3c21
eneax <19818937+eneax@users.noreply.github.com>
2018-05-11 21:33:07 +0200

    Install and config React Router

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,17 @@
 var React = require('react');
 var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className='container'>
-				<Popular />
-			</div>
+			<Router>
+				<div className='container'>
+					<Route path='/popular' component={Popular} />
+				</div>
+			</Router>
 		)
 	}
 }
