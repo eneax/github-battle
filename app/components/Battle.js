8e7b02737f0d980b2b5e64ef62bc9016716d898a
eneax <19818937+eneax@users.noreply.github.com>
2018-05-12 14:49:20 +0200

    Create PlayerPreview Component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,34 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
+function PlayerPreview () {
+  return (
+    <div>
+      <div className='column'>
+        <img
+          className='avatar'
+          src={props.avatar}
+          alt={'Avatar for ' + props.username}  
+        />
+        <h2 className='username'>@{props.username}</h2>
+      </div>
+      <button 
+        className='reset'
+        onClick={props.onReset.bind(null, props.id)}
+      >
+        Reset
+      </button>
+    </div>
+  )
+}
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired
+}
+
 class PlayerInput extends React.Component {
   constructor(props) {
     super(props);
