0b53a74b532a8cc8a073b5c374b8b1b9f957a2b5
eneax <19818937+eneax@users.noreply.github.com>
2018-05-12 09:15:55 +0200

    Config constructor and handleSubmit for Battle Component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,26 @@
 var React = require('react');
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(id, username) {
+    this.setState(function () {
+      var newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+      return newState;
+    })
+  }
   render() {
     return(
       <div>Battle</div>
