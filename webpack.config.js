e14ab0abe78074871c83b68ad906592f6067f9a6
eneax <19818937+eneax@users.noreply.github.com>
2018-05-13 16:39:45 +0200

    Fix UglifyJsPlugin error

--- webpack.config.js
+++ webpack.config.js
@@ -8,33 +8,22 @@ var config = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
     publicPath: '/'
-	},
-	module: {
+  },
+  module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-	plugins: [
+  plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
     })
-	],
-	mode: "development"
-}
-
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  )
-}
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+};
 
 module.exports = config;
