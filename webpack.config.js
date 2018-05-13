696f565564339fa218db7ead5f2f96a29da01123
eneax <19818937+eneax@users.noreply.github.com>
2018-05-13 13:58:44 +0200

    Config webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -24,3 +25,16 @@ module.exports = {
 	],
 	mode: "development"
 }
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
+
+module.exports = config;
