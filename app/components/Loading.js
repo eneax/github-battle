601f2c8f5907fbccd75e95208dc5a6315264c69c
eneax <19818937+eneax@users.noreply.github.com>
2018-05-13 11:42:44 +0200

    Add animation to Loading

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -16,6 +16,24 @@ class Loading extends React.Component {
       text: props.text
     }
   }
+  componentDidMount() {
+    var stopper = this.props.text + '...';
+    this.interval = window.setInterval(function () {
+      if (this.state.text === stopper) {
+        this.setState(function () {
+          return {
+            text: this.props.text
+          }
+        })
+      } else {
+        this.setState(function (prevState) {
+          return {
+            text: prevState.text + '.'
+          }
+        })
+      }
+    }.bind(this), 300)
+  }
   render() {
     return (
       <p style={styles.content}>
