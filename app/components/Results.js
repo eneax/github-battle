e7995d05be3ab08dbb4fa3aa7477bf7485a02b08
eneax <19818937+eneax@users.noreply.github.com>
2018-05-13 09:49:52 +0200

    Create Profile Component

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,12 +5,34 @@ var Link = require('react-router-dom').Link;
 var PropTypes = require('prop-types');
 var PlayerPreview = require('./PlayerPreview');
 
+function Profile (props) {
+  var info = props.info;
+  
+  return (
+    <PlayerPreview avatar={info.avatar_url} username={info.login} >
+      <ul className='space-list-items'>
+        {info.name && <li>{info.name}</li>}
+        {info.location && <li>{info.location}</li>}
+        {info.company && <li>{info.company}</li>}
+        <li>Followers: {info.followers}</li>
+        <li>Following: {info.following}</li>
+        <li>Public Repos: {info.public_repos}</li>
+        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+      </ul>
+    </PlayerPreview>
+  )
+}
+
+Profile.propTypes = {
+  info: PropTypes.object.isRequired 
+}
+
 function Player (props) {
   return (
     <div>
       <h1 className='header'>{props.label}</h1>
       <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
-      
+      <Profile info={props.profile}/>
     </div>
   )
 }
