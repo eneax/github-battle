f2db0aa7d2eca35c9e5185a47b5508e7a894f0ef
eneax <19818937+eneax@users.noreply.github.com>
2018-05-21 11:37:00 +0200

    Convert Results to ES6

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,12 @@
-var React = require('react');
-var queryString = require('query-string');
-var api = require('../utils/api');
-var Link = require('react-router-dom').Link;
-var PropTypes = require('prop-types');
-var PlayerPreview = require('./PlayerPreview');
-var Loading = require('./Loading');
+const React = require('react');
+const queryString = require('query-string');
+const api = require('../utils/api');
+const Link = require('react-router-dom').Link;
+const PropTypes = require('prop-types');
+const PlayerPreview = require('./PlayerPreview');
+const Loading = require('./Loading');
 
-function Profile (props) {
-  var info = props.info;
+function Profile ({ info }) {
 
   return (
     <PlayerPreview avatar={info.avatar_url} username={info.login} >
@@ -28,12 +27,12 @@ Profile.propTypes = {
   info: PropTypes.object.isRequired,
 }
 
-function Player (props) {
+function Player ({ label, score, profile }) {
   return (
     <div>
-      <h1 className='header'>{props.label}</h1>
-      <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
-      <Profile info={props.profile}/>
+      <h1 className='header'>{label}</h1>
+      <h3 style={{textAlign: 'center'}}>Score: {score}</h3>
+      <Profile info={profile}/>
     </div>
   )
 }
@@ -56,37 +55,30 @@ class Results extends React.Component {
     }
   }
   componentDidMount() {
-    var players = queryString.parse(this.props.location.search);
+    var { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
 
     api.battle([
-      players.playerOneName,
-      players.playerTwoName
-    ]).then(function (results) {
+      playerOneName,
+      playerTwoName
+    ]).then((results) => {
       if (results === null) {
-        return this.setState(function() {
-          return {
-            error: 'Looks like there was an error. Check that both users exist on Github!',
-            loading: false
-          }
-        });
+        return this.setState(() => ({
+          error: 'Looks like there was an error. Check that both users exist on Github!',
+          loading: false
+        }))
       }
 
-      this.setState(function() {
-        return {
-          error: null,
-          winner: results[0],
-          loser: results[1],
-          loading: false
-        }
-      })
-    }.bind(this))
+      this.setState(() => ({
+        error: null,
+        winner: results[0],
+        loser: results[1],
+        loading: false
+      }))
+    })
   }
   render() {
-    var error = this.state.error;
-    var winner = this.state.winner;
-    var loser = this.state.loser;
-    var loading = this.state.loading;
-
+    const { error, winner, loser, loading } = this.state;
+    
     if (loading === true) {
       return <Loading />
     }
