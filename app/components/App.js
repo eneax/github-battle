0f04944f4f083be8cd2a8db1b146d2bc537ee150
eneax <19818937+eneax@users.noreply.github.com>
2018-05-21 11:41:18 +0200

    Convert App and PlayerPreview to ES6

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,13 @@
-var React = require('react');
-var Popular = require('./Popular');
-var ReactRouter = require('react-router-dom');
-var Router = ReactRouter.BrowserRouter;
-var Route = ReactRouter.Route;
-var Switch = ReactRouter.Switch;
-var Nav = require('./Nav');
-var Home = require('./Home');
-var Battle = require('./Battle');
-var Results = require('./Results');
+const React = require('react');
+const Popular = require('./Popular');
+const ReactRouter = require('react-router-dom');
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
+const Nav = require('./Nav');
+const Home = require('./Home');
+const Battle = require('./Battle');
+const Results = require('./Results');
 
 class App extends React.Component {
 	render() {
@@ -20,9 +20,7 @@ class App extends React.Component {
 						<Route exact path='/battle' component={Battle} />
 						<Route path='/battle/results' component={Results} />
 						<Route path='/popular' component={Popular} />
-						<Route render={function() {
-							return <p>Not Found</p>
-						}} />
+						<Route render={() => <p>Not Found</p> } />
 					</Switch>
 				</div>
 			</Router>
