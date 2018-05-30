228ec038d1e174bdb3e6449ace01da6faa03d5c4
eneax <19818937+eneax@users.noreply.github.com>
2018-05-30 21:58:55 +0200

    Style Home

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 class Home extends React.Component {
   render() {
     return (
-      <div className='home-container'>
-        <h1>Battle your friends</h1>
-        <Link className='button' to='/battle'>
-          Battle
+      <header className="home tc mt6 ph3">
+        <h1 className="f2 f1-l fw2 white mb4 lh-title"><strong>Challenge Your Friends</strong></h1>
+        <Link className='button f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3' to='/battle'>
+          Battle Now
         </Link>
-      </div>
+      </header>
     )
   }
 }
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -4,16 +4,16 @@ import { NavLink } from 'react-router-dom';
 export default function Nav () {
   return (
     <nav className='dt w-100 border-box pa3 ph5-ns'>
-      <NavLink exact activeClassName='active' to='/' className='dtc v-mid near-black link dim w-25'>
+      <NavLink exact activeClassName='active' to='/' className='dtc v-mid white link dim w-25'>
         <i className="fab fa-github-alt fa-2x dib w2 h2 br-100"></i>
       </NavLink>
       
       <div className='dtc v-mid w-75 tr'>
-        <NavLink activeClassName='active' to='/battle' className='link dim near-black f6 f5-ns dib mr3 mr4-ns'>
+        <NavLink activeClassName='active' to='/battle' className='link dim white f6 f5-ns dib mr3 mr4-ns'>
           Battle
         </NavLink>
 
-        <NavLink activeClassName='active' to='/popular' className='link dim near-black f6 f5-ns dib'>
+        <NavLink activeClassName='active' to='/popular' className='link dim white f6 f5-ns dib'>
           Popular
         </NavLink>
       </div>
