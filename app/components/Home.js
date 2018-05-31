4417707566038ec0eb637e42206ee8643707ed65
eneax <19818937+eneax@users.noreply.github.com>
2018-05-31 10:18:14 +0200

    Update Header title and style it

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 class Home extends React.Component {
   render() {
     return (
-      <header className="sans-serif tc">
+      <header className="tc">
         <div className="cover bg-left bg-center-l">
           <div className="pb5 pb6-m pb7-l">
             <div className="tc-l mt4 mt5-m mt6-l ph3">
-              <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">Welcome to Github Land</h1>
+              <h1 className="f2 f1-l white-90 mb0 lh-title">Welcome to Github Battlefield</h1>
               <h2 className="fw1 f3 white-80 mt3 mb4">The only place where you can challenge your friends and discover popular repositories</h2>
               <Link className='button f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3' to='/battle'>
                 Battle Now
