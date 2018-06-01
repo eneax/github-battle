83f38d813ff68d99913832cf7d639b2abb10fbf7
eneax <19818937+eneax@users.noreply.github.com>
2018-06-01 13:55:04 +0200

    Style Loading

--- app/components/Home.js
+++ app/components/Home.js
@@ -10,11 +10,11 @@ class Home extends React.Component {
             <div className="tc-l mt5 mt6-m mt6-l ph3">
               <h1 className="f2 f1-l white-90 mb0 lh-title">Welcome <br/> to <br/> <span className='title'>Github Battlefield</span></h1>
               <h2 className="fw1 f3 white-80 mt3 mb4">The only place where you can challenge your friends and discover popular repositories</h2>
-              <Link className='button no-underline grow dib v-mid white ba b--white bw1 ph2 ph3-ns pv2 mb3' to='/battle'>
+              <Link className='button no-underline grow dib v-mid white ba b--white bw1 br2 ph2 ph3-ns pv2 mb3' to='/battle'>
                 Battle Now
               </Link> 
               <span className="dib v-mid ph3 white-70 mb3">or</span>
-              <Link className='button no-underline grow dib v-mid white ba b--white bw1 ph2 ph3-ns pv2 mb3' to='/popular'>
+              <Link className='button no-underline grow dib v-mid white ba b--white bw1 br2 ph2 ph3-ns pv2 mb3' to='/popular'>
                 Discover Repos
               </Link>
             </div>
--- app/components/Loading.js
+++ app/components/Loading.js
@@ -35,7 +35,7 @@ class Loading extends React.Component {
   }
   render() {
     return (
-      <p style={styles.content}>
+      <p className='loading' style={styles.content}>
         {this.state.text}
       </p>
     )
