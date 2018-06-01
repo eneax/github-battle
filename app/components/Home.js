1b1170871fd6b8dc4af2259c51689b622817fc01
eneax <19818937+eneax@users.noreply.github.com>
2018-06-01 18:40:45 +0200

    Remove padding bottom from Header

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,20 +6,18 @@ class Home extends React.Component {
     return (
       <header className="tc">
         <div className="cover bg-left bg-center-l">
-          <div className="pb5 pb6-m pb7-l">
-            <div className="tc-l mt5 mt6-m mt6-l ph3">
-              <h1 className="f2 f1-l white-90 mb0 lh-title">Welcome <br/> to <br/> <span className='title'>Github Battle</span></h1>
-              <h2 className="fw1 f3 white-80 mt3 mb4">The only place where you can challenge your friends and discover popular repositories</h2>
-              <Link className='button no-underline grow dib v-mid white ba b--white bw1 br2 ph2 ph3-ns pv2 mb3' to='/battle'>
-                Battle Now
-              </Link> 
-              <span className="dib v-mid ph3 white-70 mb3">or</span>
-              <Link className='button no-underline grow dib v-mid white ba b--white bw1 br2 ph2 ph3-ns pv2 mb3' to='/popular'>
-                Discover Repos
+          <div className="tc-l mt5 mt6-m mt6-l ph3">
+            <h1 className="f2 f1-l white-90 mb0 lh-title">Welcome <br /> to <br /> <span className='title'>Github Battle</span></h1>
+            <h2 className="fw1 f3 white-80 mt3 mb4">The only place where you can challenge your friends and discover popular repositories</h2>
+            <Link className='button no-underline grow dib v-mid white ba b--white bw1 br2 ph2 ph3-ns pv2 mb3' to='/battle'>
+              Battle Now
               </Link>
-            </div>
+            <span className="dib v-mid ph3 white-70 mb3">or</span>
+            <Link className='button no-underline grow dib v-mid white ba b--white bw1 br2 ph2 ph3-ns pv2 mb3' to='/popular'>
+              Discover Repos
+              </Link>
           </div>
-        </div> 
+        </div>
       </header>
     )
   }
