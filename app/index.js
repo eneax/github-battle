cfbeb306e00415bfd739ffb96a8f49cd7161d0b8
eneax <19818937+eneax@users.noreply.github.com>
2019-10-22 21:24:18 +0200

    Import Popular in app

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './index.css'
+import Popular from './components/Popular'
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello World! 😄</div>
+      <div className="container">
+        <Popular />
+      </div>
     )
   }
 }
