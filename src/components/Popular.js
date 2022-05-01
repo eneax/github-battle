fe336109654dd333c2284ab0340effd52e4985ca
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-05-01 13:03:23 +0200

    Caching repos

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -34,7 +34,7 @@ export default class Popular extends React.Component {
 
     this.state = {
       selectedLanguage: "All",
-      repos: null,
+      repos: {},
       error: null,
     };
 
@@ -49,28 +49,33 @@ export default class Popular extends React.Component {
   updateLanguage(selectedLanguage) {
     this.setState({
       selectedLanguage,
-      repos: null,
       error: null,
     });
 
-    fetchPopularRepos(selectedLanguage)
-      .then((repos) => {
-        this.setState({
-          repos,
-          error: null,
-        });
-      })
-      .catch((err) => {
-        console.warn("Error fetching repos: ", err);
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then((data) => {
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data,
+            },
+          }));
+        })
+        .catch((err) => {
+          console.warn("Error fetching repos: ", err);
 
-        this.setState({
-          error: "There was an error fetching the repositories.",
+          this.setState({
+            error: "There was an error fetching the repositories.",
+          });
         });
-      });
+    }
   }
 
   isLoading() {
-    return this.state.repos === null && this.state.error === null;
+    const { selectedLanguage, repos, error } = this.state;
+
+    return !repos[selectedLanguage] && error === null;
   }
 
   render() {
@@ -87,7 +92,9 @@ export default class Popular extends React.Component {
 
         {error && <p>{error}</p>}
 
-        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+        {repos[selectedLanguage] && (
+          <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>
+        )}
       </React.Fragment>
     );
   }
