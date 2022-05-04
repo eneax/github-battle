23fc179778a79de3c187a1a3f4f452647ff37627
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-05-04 16:02:44 +0200

    Reset Players

--- src/components/Results.js
+++ src/components/Results.js
@@ -96,29 +96,41 @@ export default class Results extends React.Component {
     }
 
     return (
-      <div className="grid space-around container-sm">
-        {/* Winner */}
-        <Card
-          header={winner.score === loser.score ? "Tie" : "Winner"}
-          subheader={`Score: ${winner.score.toLocaleString()}`}
-          avatar={winner.profile.avatar_url}
-          href={winner.profile.html_url}
-          name={winner.profile.login}
-        >
-          <ProfileList profile={winner.profile} />
-        </Card>
+      <React.Fragment>
+        <div className="grid space-around container-sm">
+          {/* Winner */}
+          <Card
+            header={winner.score === loser.score ? "Tie" : "Winner"}
+            subheader={`Score: ${winner.score.toLocaleString()}`}
+            avatar={winner.profile.avatar_url}
+            href={winner.profile.html_url}
+            name={winner.profile.login}
+          >
+            <ProfileList profile={winner.profile} />
+          </Card>
 
-        {/* Loser */}
-        <Card
-          header={winner.score === loser.score ? "Tie" : "Loser"}
-          subheader={`Score: ${loser.score.toLocaleString()}`}
-          avatar={loser.profile.avatar_url}
-          name={loser.profile.login}
-          href={loser.profile.html_url}
-        >
-          <ProfileList profile={loser.profile} />
-        </Card>
-      </div>
+          {/* Loser */}
+          <Card
+            header={winner.score === loser.score ? "Tie" : "Loser"}
+            subheader={`Score: ${loser.score.toLocaleString()}`}
+            avatar={loser.profile.avatar_url}
+            name={loser.profile.login}
+            href={loser.profile.html_url}
+          >
+            <ProfileList profile={loser.profile} />
+          </Card>
+        </div>
+
+        <button className="btn dark-btn btn-space" onClick={this.props.onReset}>
+          Reset
+        </button>
+      </React.Fragment>
     );
   }
 }
+
+Results.propTypes = {
+  playerOne: PropTypes.string.isRequired,
+  playerTwo: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+};
