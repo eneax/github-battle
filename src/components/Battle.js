a9d2624ad2126af3c0055cab33574124d20d8f3b
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-05-04 11:39:53 +0200

    Create initial Results component

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -7,6 +7,8 @@ import {
   FaTimesCircle,
 } from "react-icons/fa";
 
+import Results from "./Results";
+
 function Instructions() {
   return (
     <div className="instructions-container">
@@ -127,6 +129,7 @@ export default class Battle extends React.Component {
     this.state = {
       playerOne: null,
       playerTwo: null,
+      battle: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -146,7 +149,11 @@ export default class Battle extends React.Component {
   }
 
   render() {
-    const { playerOne, playerTwo } = this.state;
+    const { playerOne, playerTwo, battle } = this.state;
+
+    if (battle === true) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />;
+    }
 
     return (
       <React.Fragment>
@@ -181,6 +188,15 @@ export default class Battle extends React.Component {
               />
             )}
           </div>
+
+          {playerOne && playerTwo && (
+            <button
+              className="btn dark-btn btn-space"
+              onClick={() => this.setState({ battle: true })}
+            >
+              Battle
+            </button>
+          )}
         </div>
       </React.Fragment>
     );
