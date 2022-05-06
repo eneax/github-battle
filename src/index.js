4e7117c51dacb02b4f8e4cea4b4edef20b229025
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-05-06 11:34:31 +0200

    Implement toggle theme functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 
 import { ThemeProvider } from "./context/theme";
+import Nav from "./components/Nav";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
 
@@ -24,8 +25,11 @@ class App extends React.Component {
   render() {
     return (
       <ThemeProvider value={this.state}>
-        <div className="container">
-          <Battle />
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+            <Battle />
+          </div>
         </div>
       </ThemeProvider>
     );
