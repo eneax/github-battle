0c0ea1d36d1b1bcc338bd0c49ae5f73403e401d9
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-05-06 14:52:03 +0200

    Add routes

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js",
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -25,4 +26,7 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
+  devServer: {
+    historyApiFallback: true,
+  },
 };
